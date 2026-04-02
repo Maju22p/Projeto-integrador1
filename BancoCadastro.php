bc9d66e589667ab20b1714eb7bcde85e389b7124
Maju22p <maju22spedro@gmail.com>
2026-04-02 05:10:17 -0300

    modified:   BancoCadastro.php

--- BancoCadastro.php
+++ BancoCadastro.php
@@ -4,11 +4,14 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// Carrega as variáveis de ambiente do arquivo .env
+
 $dbHost = $_ENV['DB_HOST'];
 $dbName = $_ENV['DB_NAME2'];
 $dbUser = $_ENV['DB_USER'];
 $dbPass = $_ENV['DB_PASS'];
 
+// Conecta ao banco de dados MySQL e cria o banco de dados se ele não existir
 try {
     $pdo = new PDO("mysql:host=$dbHost", $dbUser, $dbPass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -22,16 +25,14 @@ try {
 try {
     $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    // Cria a tabela "usuarios" se ela não existir
     $sql = "CREATE TABLE IF NOT EXISTS usuarios(
         id INT AUTO_INCREMENT PRIMARY KEY,
         usuario VARCHAR(255) NOT NULL UNIQUE,
-        nome VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL UNIQUE,
-        senha VARCHAR(255) NOT NULL,
-        data_nascimento DATE NOT NULL,
-        genero ENUM('Masculino', 'Feminino', 'Outro') NOT NULL
+        senha VARCHAR(255) NOT NULL
     )";
+
     $pdo->exec($sql);
 } catch (PDOException $e) {
     die("Erro ao conectar ao banco de dados: " . $e->getMessage());
